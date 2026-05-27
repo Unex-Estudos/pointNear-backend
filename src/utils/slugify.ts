a38035810f432676cafd51c69360ae40d577f390
Cleyviton Ferreira <cleyviton@espressolabs.com.br>
2026-05-26 21:55:28 -0300

    hot: clear project

--- src/utils/slugify.ts
+++
@@ -1,9 +0,0 @@
-export function slugify(value: string): string {
-  return value
-    .normalize('NFD')
-    .replace(/[̀-ͯ]/g, '')
-    .toLowerCase()
-    .trim()
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-+|-+$/g, '');
-}
